9ce1223ff81b4548e7a353af96100e56c07e4577
ZxOnTheGitHub <77508900+ZxOnTheGitHub@users.noreply.github.com>
2021-01-24 19:25:10 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,8 +2,7 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-client.once('ready', function () {
-        console.log('ready !');
+client.login(process.env.TOKEN);
+
     });
 
-client.login('ODAyOTQ5MDg3NjgyNDk0NDk1.YA2qnA.8P-6fTc-uG8xJxCj6-habYjGumc');
