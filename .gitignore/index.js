f1b2b555f26784ea5b647531ed85b4d9e7278ef5
ZxOnTheGitHub <77508900+ZxOnTheGitHub@users.noreply.github.com>
2021-01-24 18:59:57 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', function () {
+        console.log('ready !');
+    });
+
+client.login('NzkyMDEzNzc4NDQyOTExNzY0.X-XiTw.ATHbGyzdQ7o73Das0rh1qbEGs6I');
